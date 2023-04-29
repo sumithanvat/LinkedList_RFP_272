cc2085b652a6bf903206fc87c16304149dcfb77e
sumithanvat <skhanvat@gmail.com>
2023-04-30 00:09:56 +0530

    Added:Ability to create Linked List by appending 30 and 70 to 56

--- src/linkedlist/Main.java
+++ src/linkedlist/Main.java
@@ -2,9 +2,9 @@ package linkedlist;
 public class Main {
     public static void main(String[] args) {
         // creating object of linked list
-        linkedList.add(70); // First create node with data 70
-        linkedList.add(30); // Then add node with data 30 to the list
-        linkedList.add(56);
+        linkedList.add(56); // First create node with data 56
+        linkedList.add(30); // Then append node with data 30 to the list
+        linkedList.add(70); // Finally append node with data 70 to the list
         linkedList.display();
-        }
+    }
     }
