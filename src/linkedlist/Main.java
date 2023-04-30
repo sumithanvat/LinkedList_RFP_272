ff1b466eb0aa0f1963c53e222990a2e477677bc8
sumithanvat <skhanvat@gmail.com>
2023-04-30 20:28:14 +0530

    Added:Ability to delete the last element in the LinkedList of sequence 56->30->70

--- src/linkedlist/Main.java
+++ src/linkedlist/Main.java
@@ -1,10 +1,11 @@
 package linkedlist;
 public class Main {
     public static void main(String[] args) {
+
         linkedList.add(56); // First create node with data 56
         linkedList.add(30); // Then add node with data 30 to the list
         linkedList.add(70); // Finally add node with data 70 to the list
-        linkedList.pop(); // Delete the first node with data 56
-        linkedList.display(); // Output: 30 -> 70 -> null
+        linkedList.popLast(); // Delete the last node with data 70
+        linkedList.display();
     }
     }
--- src/linkedlist/linkedList.java
+++ src/linkedlist/linkedList.java
@@ -2,16 +2,15 @@ package linkedlist;
 
 public class linkedList {
     static Node head; // Reference to the first node
+    static Node tail; // Reference to the last node
 
     public static void add(int data) { // Add a new node to the end of the list
         if (head == null) {
             head = new Node(data); // If list is empty, create the first node
+            tail = head; // Update the tail to the new node
         } else {
-            Node current = head;
-            while (current.next != null) {
-                current = current.next; // Traverse the list to get to the end
-            }
-            current.next = new Node(data); // Add the new node to the end
+            tail.next = new Node(data); // Add the new node to the end
+            tail = tail.next; // Update the tail to the new node
         }
     }
 
@@ -22,6 +21,9 @@ public class linkedList {
                 Node newNode = new Node(data); // Create the new node
                 newNode.next = current.next; // Point the new node to the next node
                 current.next = newNode; // Point the current node to the new node
+                if (tail == current) {
+                    tail = newNode; // Update the tail if the new node is added at the end
+                }
                 break;
             }
             current = current.next; // Move to the next node
@@ -33,6 +35,26 @@ public class linkedList {
             head = head.next; // Update the head to the next node
         }
     }
+
+    public static int popLast() { // Delete the last node and return its value
+        if (head == null) { // If the list is empty
+            return -1; // Return -1 to indicate that the list is empty
+        } else if (head == tail) { // If there's only one node in the list
+            int value = head.data;
+            head = null;
+            tail = null;
+            return value;
+        } else { // If there are more than one nodes in the list
+            Node current = head;
+            while (current.next != tail) {
+                current = current.next; // Traverse the list to get to the second-to-last node
+            }
+            int value = tail.data;
+            tail = current; // Update the tail to the second-to-last node
+            tail.next = null;
+            return value;
+        }
+    }
     public static void display() { // Display the values in the list
         Node current = head;
         while (current != null) {
@@ -41,4 +63,5 @@ public class linkedList {
         }
         System.out.println("null");
     }
+
 }
